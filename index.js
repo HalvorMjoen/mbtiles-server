5350994f7ebf02f0d0adb7bf17ab9a8a9a4e0a3a
HalvorMjoen <halvor@visbook.no>
2018-12-07 12:46:25 +0100

    added some mbtiles

--- index.js
+++ index.js
@@ -44,38 +44,40 @@ function errorHeader() {
 }
 
 // tile cannon
-app.get("/:s/:z/:x/:y.:t", function (req, res) {
-  if (fs.existsSync(req.params.s + ".mbtiles")) {
-    new MBTiles(p.join(tilesDir, req.params.s + ".mbtiles"), function (
-      err,
-      mbtiles
-    ) {
-      if (err) {
-        // Database read error
-        res.set(errorHeader());
-        res.status(404).send("Error opening database: " + err + "\n");
-      } else {
-        mbtiles.getTile(req.params.z, req.params.x, req.params.y, function (
-          err,
-          tile,
-          headers
-        ) {
-          if (err) {
-            // Tile read error
-            res.set(errorHeader());
-            res.status(404).send("Tile rendering error: " + err + "\n");
-          } else {
-            res.set(getContentType(req.params.t));
-            res.send(tile);
-          }
-        });
-      }
-    });
-  } else {
-    // Database not found error
-    res.set(errorHeader());
-    res.status(404).send("MBTILES database not found.");
-  }
+app.get('/:z/:x/:y.*', function (req, res) {
+  //  if (fs.existsSync(req.params.s + ".mbtiles")) {
+  new MBTiles(p.join(tilesDir, "33_N5000raster_1.mbtiles"), function (
+    err,
+    mbtiles
+  ) {
+    if (err) {
+      // Database read error
+      res.set(errorHeader());
+      res.status(404).send("Error opening database: " + err + "\n");
+    } else {
+      mbtiles.getTile(req.params.z, req.params.x, req.params.y, function (
+        err,
+        tile,
+        headers
+      ) {
+        if (err) {
+          // Tile read error
+          console.log("Tiles not found on z:" +  req.params.z + " x: " + req.params.x + " y: " + req.params.y);
+          res.set(errorHeader());
+          res.status(404).send("Tile rendering error: " + err + "\n");
+        } else {
+          console.log("Tiles found");
+          res.set(getContentType(req.params.t));
+          res.send(tile);
+        }
+      });
+    }
+  });
+  // } else {
+  //   // Database not found error
+  //   res.set(errorHeader());
+  //   res.status(404).send("MBTILES database not found.");
+  // }
 });
 
 // start up the server
